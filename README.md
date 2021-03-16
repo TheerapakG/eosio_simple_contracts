fee39edb80e8c5ab9754e5906812ca6715877229
TheerapakG <theerapakg@gmail.com>
2021-03-16 20:14:35 +0700

    finished variable reserve swap contract

---
+++ README.md
@@ -0,0 +1,2 @@
+Various simple ready-to-use eosio contracts.
+Not audited - use at your own risk!
--- include/simple_contracts/swap.hpp
+++ include/simple_contracts/swap.hpp
@@ -9,38 +9,12 @@
 
 namespace simple_contracts
 {
-    template<typename T>
-    class Reserve
+    CONTRACT VariableReserveSwapContract: public contract
     {
         public:
-        class TokenPair
-        {
-            public:
-            auto swap(name owner, const Token& token, uint64_t amount) -> void
-            {
-                static_cast<typename T::TokenPair*>(this)->swap(token, amount);
-            }
-        };
+        using contract::contract;
 
-        auto get_pair(const Token& token1, const Token& token2) //-> typename T::TokenPair
-        {
-            return static_cast<T*>(this)->get_pair(token1, token2);
-        }
-    };
-
-#ifdef CHECKER
-    template <typename Base = contract>
-    class VariableReserveContractMixin: public Reserve<VariableReserveContractMixin<Base>>, public Base
-#else
-    template <typename Base, std::enable_if_t<std::is_base_of_v<contract, Base>, int> = 0>
-    class VariableReserveContractMixin: public Reserve<VariableReserveContractMixin<Base>>, public Base
-#endif
-    {
-        public:
-        using Base::Base;
-        using contract_t = VariableReserveContractMixin<Base>;
-
-        TABLE TokenTable_row
+        TABLE tokens
         {
             uint64_t id;
             Token    token;
@@ -49,7 +23,7 @@ namespace simple_contracts
             auto token_key()   const -> uint128_t { return token.value(); }
         };
 
-        TABLE PairTable_row
+        TABLE pairs
         {
             uint64_t id;
             uint64_t token1_id;
@@ -62,7 +36,7 @@ namespace simple_contracts
             auto token_pair_key() const -> uint128_t { return token1_id * (static_cast<uint128_t>(std::numeric_limits<uint64_t>::max())+1) + token2_id; }
         };
 
-        TABLE LiquidityTable_row
+        TABLE liquidity
         {
             uint64_t id;
             uint64_t token1_staged;
@@ -72,19 +46,21 @@ namespace simple_contracts
             auto primary_key()    const -> uint64_t  { return id; }
         };
         
-        using TokenTable = multi_index<name{"token"}, TokenTable_row,
-            indexed_by<name{"token"}, const_mem_fun<TokenTable_row, uint128_t, &TokenTable_row::token_key>>
+        using TokenTable = multi_index<name{"tokens"}, tokens,
+            indexed_by<name{"token"}, const_mem_fun<tokens, uint128_t, &tokens::token_key>>
         >;
-        using PairTable = multi_index<name{"pair"}, PairTable_row,
-            indexed_by<name{"tokenpair"}, const_mem_fun<PairTable_row, uint128_t, &PairTable_row::token_pair_key>>
+        using PairTable = multi_index<name{"pairs"}, pairs,
+            indexed_by<name{"tokenpair"}, const_mem_fun<pairs, uint128_t, &pairs::token_pair_key>>
         >;
-        using LiquidityTable = multi_index<name{"liquidity"}, LiquidityTable_row>; 
+        using LiquidityTable = multi_index<name{"liquidity"}, liquidity>; 
 
-        TokenTable token_table{Base::get_self(), Base::get_self().value};
-        PairTable  pair_table {Base::get_self(), Base::get_self().value};
+        TokenTable token_table{get_self(), get_self().value};
+        PairTable  pair_table {get_self(), get_self().value};
+
+        class TokenPair;
 
         private:
-        friend class contract_t::TokenPair;
+        friend class VariableReserveSwapContract::TokenPair;
 
         auto get_token_id(const Token& token) const
         {
@@ -98,20 +74,20 @@ namespace simple_contracts
             {
                 Token token = token_table.find(token_id)->token;
                 action{
-                    permission_level{Base::get_self(), name{"active"}}, 
+                    permission_level{get_self(), name{"active"}}, 
                     token.get_contract(),
                     name{"transfer"},
-                    std::make_tuple(Base::get_self(), owner, asset{amount, token.get_symbol()}, string{})
+                    std::make_tuple(get_self(), owner, asset{amount, token.get_symbol()}, string{})
                 }.send();
             }
         }
 
         public:
-        class TokenPair: public Reserve<contract_t>::TokenPair
+        class TokenPair
         {
             private:
-            contract_t& outer;
-            typename PairTable::const_iterator  it;
+            VariableReserveSwapContract& outer;
+            PairTable::const_iterator    it;
 
             auto reserve_stake(uint64_t liquidity_amount) const -> std::pair<uint64_t, uint64_t>
             {
@@ -128,21 +104,23 @@ namespace simple_contracts
             }
 
             public:
-            TokenPair(contract_t& outer, uint64_t token1_id, uint64_t token2_id): 
+            TokenPair(VariableReserveSwapContract& outer, uint64_t token1_id, uint64_t token2_id): 
                 outer(outer), 
-                it(
-                    outer.pair_table
-                    .template get_index<name{"tokenpair"}>()
-                    .require_find(token1_id * (static_cast<uint128_t>(std::numeric_limits<uint64_t>::max())+1) + token2_id, "token pair not created")
-                )
+                it{
+                    outer.pair_table.iterator_to(
+                        outer.pair_table
+                        .template get_index<name{"tokenpair"}>()
+                        .get(token1_id * (static_cast<uint128_t>(std::numeric_limits<uint64_t>::max())+1) + token2_id, "token pair not created")
+                    )
+                }
             {}
 
-            TokenPair(contract_t& outer, uint64_t pair_id): 
+            TokenPair(VariableReserveSwapContract& outer, uint64_t pair_id): 
                 outer(outer), 
-                it(
+                it{
                     outer.pair_table
                     .require_find(pair_id, "token pair id not exist")
-                )
+                }
             {}
 
             auto stage_token(name owner, uint64_t token_id, uint64_t amount) -> void
@@ -151,10 +129,12 @@ namespace simple_contracts
                 auto lt_it = liquidity_table.find(it->id);
                 if(lt_it==liquidity_table.end())
                 {
+                    uint64_t last_id = ((liquidity_table.begin()==liquidity_table.end())? 0:liquidity_table.rbegin()->id);
                     liquidity_table.emplace(
                         outer.get_self(),
-                        [&](LiquidityTable_row& row)
+                        [&](liquidity& row)
                         {
+                            row.id = last_id + 1;
                             row.token1_staged = (token_id == it->token1_id? amount:0);
                             row.token2_staged = (token_id == it->token2_id? amount:0);
                             row.liquidity_amount = 0;
@@ -166,7 +146,7 @@ namespace simple_contracts
                     liquidity_table.modify(
                         liquidity_table.find(it->id),
                         same_payer,
-                        [&](LiquidityTable_row& row)
+                        [&](liquidity& row)
                         {
                             if(token_id == it->token1_id) row.token1_staged += amount;
                             if(token_id == it->token2_id) row.token2_staged += amount;
@@ -199,7 +179,7 @@ namespace simple_contracts
                     liquidity_table.modify(
                         liquidity_table.find(it->id),
                         same_payer,
-                        [&](LiquidityTable_row& row)
+                        [&](liquidity& row)
                         {
                             row.token1_staged -= amount1;
                             row.token2_staged -= amount2;
@@ -214,7 +194,7 @@ namespace simple_contracts
                 outer.pair_table.modify(
                     it, 
                     same_payer,
-                    [&](PairTable_row& row)
+                    [&](pairs& row)
                     {
                         row.token1_reserve += r_amount1;
                         row.token2_reserve += r_amount2;
@@ -232,7 +212,7 @@ namespace simple_contracts
                 outer.pair_table.modify(
                     it, 
                     same_payer,
-                    [amount1=amount1, amount2=amount2, liquidity_amount](PairTable_row& row)
+                    [amount1=amount1, amount2=amount2, liquidity_amount](pairs& row)
                     {
                         row.token1_reserve -= amount1;
                         row.token2_reserve -= amount2;
@@ -241,12 +221,12 @@ namespace simple_contracts
                 );
             }
 
-            auto swap(name owner, const Token& token, uint64_t amount) -> void
+            auto swap(name owner, uint64_t token_id, uint64_t amount) -> void
             {
                 uint64_t token1_new, token2_new;
                 uint64_t swapped_token, swapped_amount;
 
-                if(outer.get_token_id(token)==it->token1_id)
+                if(token_id==it->token1_id)
                 {
                     uint64_t token1_reserve = it->token1_reserve, token2_reserve = it->token2_reserve;
                     token1_new = token1_reserve + amount;
@@ -255,7 +235,7 @@ namespace simple_contracts
                     swapped_token = it->token2_id;
                     swapped_amount = token2_reserve - token2_new;
                 }
-                else if(outer.get_token_id(token)==it->token2_id)
+                else if(token_id==it->token2_id)
                 {
                     uint64_t token1_reserve = it->token1_reserve, token2_reserve = it->token2_reserve;
                     token2_new = token2_reserve + amount;
@@ -271,7 +251,7 @@ namespace simple_contracts
                 outer.pair_table.modify(
                     it, 
                     same_payer,
-                    [&](PairTable_row& row)
+                    [&](pairs& row)
                     {
                         row.token1_reserve = token1_new;
                         row.token2_reserve = token2_new;
@@ -282,10 +262,10 @@ namespace simple_contracts
 
         auto add_token(const Token& token) -> void
         {
-            uint64_t last_id = token_table.begin()==token_table.end()? 0:token_table.rbegin()->id;
+            uint64_t last_id = ((token_table.begin()==token_table.end())? 0:token_table.rbegin()->id);
             token_table.emplace(
-                Base::get_self(), 
-                [&](TokenTable_row& row)
+                get_self(), 
+                [&](tokens& row)
                 {
                     row.id = last_id+1;
                     row.token = token;
@@ -297,10 +277,10 @@ namespace simple_contracts
         {
             uint64_t token1_id = get_token_id(token1), token2_id = get_token_id(token2);
 
-            uint64_t last_id = pair_table.begin()==pair_table.end()? 0:pair_table.rbegin()->id;
+            uint64_t last_id = ((pair_table.begin()==pair_table.end())? 0:pair_table.rbegin()->id);
             pair_table.emplace(
-                Base::get_self(), 
-                [&](PairTable_row& row)
+                get_self(), 
+                [&](pairs& row)
                 {
                     row.id = last_id+1;
                     row.token1_id = token1_id;
@@ -327,21 +307,33 @@ namespace simple_contracts
         [[eosio::on_notify("*::transfer")]] 
         void on_reserve_transfer(name user, name receiver, asset value, string memo)
         {
-            if(user == Base::get_self()) return; // our tx
+            if(user == get_self()) return; // our tx
 
-            name token_contract = Base::get_first_receiver();
+            name token_contract = get_first_receiver();
             
             if(memo.empty()) return; // assume donation
-            if(memo[0] != 'D') return; // not deposit
+            if(memo[0] == 'D')
+            {
+                uint64_t token_id = get_token_id(Token{{value.symbol, token_contract}});
 
-            uint64_t token_id = get_token_id(Token{{value.symbol, token_contract}});
+                // deposit
+                string rest = memo.substr(1);
+                uint64_t pair_id = std::stoull(std::string{rest.begin(), rest.end()}); // TODO: illegal conversion?
 
-            // deposit
-            string rest = memo.substr(1);
-            uint64_t pair_id = std::stoull(std::string{rest.begin(), rest.end()}); // TODO: illegal conversion?
+                TokenPair pair = get_pair(pair_id);
+                pair.stage_token(user, token_id, value.amount);
+            }
+            if(memo[0] == 'S')
+            {
+                uint64_t token_id = get_token_id(Token{{value.symbol, token_contract}});
 
-            TokenPair pair = get_pair(pair_id);
-            pair.stage_token(user, token_id, value.amount);
+                // deposit
+                string rest = memo.substr(1);
+                uint64_t pair_id = std::stoull(std::string{rest.begin(), rest.end()}); // TODO: illegal conversion?
+
+                TokenPair pair = get_pair(pair_id);
+                pair.swap(user, token_id, value.amount);
+            }
         }
 
         ACTION addtoken(eosio::name contract, eosio::symbol symbol)
@@ -364,7 +356,7 @@ namespace simple_contracts
         {
             require_auth(owner);
 
-            LiquidityTable liquidity_table{Base::get_self(), owner.value};
+            LiquidityTable liquidity_table{get_self(), owner.value};
             auto lt_it = liquidity_table.require_find(pair_id, "no asset deposited to the pair");
             TokenPair pair = get_pair(pair_id);
             pair.deposit(owner, lt_it->token1_staged, lt_it->token2_staged);
@@ -374,7 +366,7 @@ namespace simple_contracts
         {
             require_auth(owner);
 
-            LiquidityTable liquidity_table{Base::get_self(), owner.value};
+            LiquidityTable liquidity_table{get_self(), owner.value};
             auto lt_it = liquidity_table.require_find(pair_id, "no asset deposited to the pair");
 
             check(liquidity_amount <= lt_it->liquidity_amount, "insufficient liquidity");
